5490d1b3e9cf9847e15fc703fe8cbc0e59cbef5d
vinay-incubyte <vinay@incubyte.co>
2025-09-01 12:00:15 +0530

    feat(presentation): verfiy login button when valid credentials - test pass (GREEN)

--- lib/feature/login/presentation/view/login_page.dart
+++ lib/feature/login/presentation/view/login_page.dart
@@ -11,32 +11,48 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage>
     with EmailValidationMixin, PasswordValidationMixin {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  @override
+  void dispose() {
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
-      body: Column(
-        children: [
-          TextFormField(
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            validator: (value) {
-              if (value == null) return null;
-              final isvalidEmail = validateEmail(value);
-              return isvalidEmail ? null : "Enter a vaild email";
-            },
-          ),
-          TextFormField(
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            validator: (value) {
-              if (value == null) return null;
-              final isValidPass = validatePassword(value);
-              return isValidPass
-                  ? null
-                  : 'Password should be -\nMin 2 char long\nContain upper and lower case char\nContain special char';
-            },
-          ),
-          ElevatedButton(onPressed: null, child: Text('Login ->')),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          spacing: 20,
+          children: [
+            TextFormField(
+              controller: email,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (value) {
+                if (value == null) return null;
+                final isvalidEmail = validateEmail(value);
+                return isvalidEmail ? null : "Enter a vaild email";
+              },
+            ),
+            TextFormField(
+              controller: password,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (value) {
+                if (value == null) return null;
+                final isValidPass = validatePassword(value);
+                return isValidPass
+                    ? null
+                    : 'Password should be -\nMin 2 char long\nContain upper and lower case char\nContain special char';
+              },
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Login ->')),
+            Text("Login success"),
+          ],
+        ),
       ),
     );
   }
