6db87892c3573109ea7a8594eb0a16eb929c6ad7
vinay-incubyte <vinay@incubyte.co>
2025-09-01 11:51:01 +0530

    test(presentation): verfiy login button when valid credentials - test failed (RED)

--- test/feature/login/presentation/view/login_page_test.dart
+++ test/feature/login/presentation/view/login_page_test.dart
@@ -60,10 +60,7 @@ void main() {
       await tester.pumpWidget(loadPage());
       await tester.pumpAndSettle();
       //act
-      await tester.enterText(
-        find.byType(TextFormField).at(1),
-        "1234Ab@",
-      );
+      await tester.enterText(find.byType(TextFormField).at(1), "1234Ab@");
       await tester.pumpAndSettle();
       // assert
       expect(find.byType(TextFormField).at(1), findsOne);
@@ -75,14 +72,35 @@ void main() {
       await tester.pumpWidget(loadPage());
       await tester.pumpAndSettle();
       //act
-      await tester.enterText(
-        find.byType(TextFormField).at(1),
-        "1234",
-      );
+      await tester.enterText(find.byType(TextFormField).at(1), "1234");
       await tester.pumpAndSettle();
       // assert
       expect(find.byType(TextFormField).at(1), findsOne);
-      expect(find.text('Password should be -\nMin 2 char long\nContain upper and lower case char\nContain special char'), findsOne);
+      expect(
+        find.text(
+          'Password should be -\nMin 2 char long\nContain upper and lower case char\nContain special char',
+        ),
+        findsOne,
+      );
+    });
+
+    testWidgets('verfiy login button when valid credentials', (tester) async {
+      // arrange
+      await tester.pumpWidget(loadPage());
+      await tester.pumpAndSettle();
+      //act
+      await tester.enterText(
+        find.byType(TextFormField).at(0),
+        "vinay@incubyte.co",
+      );
+      await tester.pumpAndSettle();
+      await tester.enterText(find.byType(TextFormField).at(1), "1234Ab@");
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      // assert
+      expect(find.byType(ElevatedButton), findsOne);
+      expect(find.text('Login success'), findsOne);
     });
   });
 }
