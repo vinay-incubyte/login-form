55cc34e54e6a2fea6ccb15969b76dc974e570f02
vinay-incubyte <vinay@incubyte.co>
2025-09-01 10:22:24 +0530

    feat(core) email validation mixin -verify invalid email - test pass (GREEN)

--- lib/core/validations/email_validation_mixin.dart
+++ lib/core/validations/email_validation_mixin.dart
@@ -1,5 +1,8 @@
 mixin EmailValidationMixin {
   bool validateEmail(String email) {
-    return true;
+    final emailRegex = RegExp(
+      r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$',
+    );
+    return emailRegex.hasMatch(email);
   }
 }
