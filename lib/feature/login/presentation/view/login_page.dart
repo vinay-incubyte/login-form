7b160fb7bf8948bc35b01c52fd2e834b1fddfef9
vinay-incubyte <vinay@incubyte.co>
2025-09-01 09:54:54 +0530

    feat(presentation): login page title - test passed (GREEN)

--- lib/feature/login/presentation/view/login_page.dart
+++ lib/feature/login/presentation/view/login_page.dart
@@ -10,6 +10,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(title: Text('Login')));
   }
 }
